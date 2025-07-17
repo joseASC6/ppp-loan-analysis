45a0a80fd8be216cf1a56dd2ccf993e446262b47
joseASC6 <jose.mauricio0226@gmail.com>
2025-07-16 23:39:24 -0400

    Add function to load data into PostgreSQL
    Introduces load_to_PostgreSQL to upload dimension and fact tables from Azure blobs to a PostgreSQL database. Handles multiple tables, including dim and facts, and provides status messages for each upload step.

---
+++ pipeline/load/load.py
@@ -0,0 +1,76 @@
+from sqlalchemy import create_engine
+import pandas as pd
+from utils.common import upload_to_sql, df_to_bytesio, download_from_azure, get_blob_list
+import io
+
+def load_to_PostgreSQL():
+    """
+    Upload a dimensions and facts to PostgreSQL database.
+        - dim_loan_status
+        - dim_processing_method
+        - dim_business_type
+        - dim_sba_office
+        - dim_term
+        - dim_business_age
+        - dim_originating_lender
+        - dim_borrower
+        - dim_servicing_lender
+        - facts_ppp
+
+        - dim_date
+
+        - facts_gdp
+        - dim_geography
+    """
+
+    final_container = "final-data"
+
+    dimensions = [
+        "dim_loan_status",
+        "dim_processing_method",
+        "dim_business_type",
+        "dim_sba_office",
+        "dim_term",
+        "dim_business_age",
+        "dim_originating_lender",
+        "dim_borrower",
+        "dim_servicing_lender",
+        "dim_date",
+        "dim_geography"
+    ]
+
+    facts = [
+        "facts_ppp",
+        "facts_gdp"
+    ]
+
+    # Upload dimensions to PostgreSQL
+    for dim in dimensions:
+        data = download_from_azure(blob_name=f"{dim}.csv", container_name=final_container)
+        df = pd.read_csv(data, encoding="utf-8")
+        upload_to_sql(df=df, table_name=f"{dim}")
+
+    print("All dimensions uploaded to PostgreSQL successfully.\n")
+
+    # Upload facts_gdp to PostgreSQL
+    gdp_data = download_from_azure(blob_name="facts_gdp.csv", container_name=final_container)
+    df = pd.read_csv(gdp_data, encoding="utf-8")
+    upload_to_sql(df=df, table_name="facts_gdp")
+
+    print("All facts_gdp uploaded to PostgreSQL successfully.\n")
+
+    # Get the list of blobs in the final container /facts_ppp
+    ppp_blobs = get_blob_list(final_container, prefix="facts_ppp")
+    if not ppp_blobs:
+        print("No facts_ppp blobs found.")
+        return
+    print(f"Found {len(ppp_blobs)} facts_ppp blobs.")
+    for blob_name in ppp_blobs:
+        data = download_from_azure(blob_name=blob_name, container_name=final_container)
+        df = pd.read_csv(data, encoding="utf-8")
+        upload_to_sql(df=df, table_name="facts_ppp")
+    print("All facts_ppp uploaded to PostgreSQL successfully.\n")
+
+
+
+    print("All data loaded to PostgreSQL successfully.\n")
