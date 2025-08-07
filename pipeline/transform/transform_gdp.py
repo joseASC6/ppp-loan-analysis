0c4784c9799e1f2607daddea1dbbb3036d3cd582
joseASC6 <jose.mauricio0226@gmail.com>
2025-08-07 19:50:04 -0400

    Refactor GDP transform to use generic cloud utils
    Replaces Azure-specific utility functions with generic cloud storage functions for downloading and uploading data. Extracts creation of 'dim_geography' and 'facts_gdp' into separate functions for improved modularity. Updates configuration to use constants from the config module.

--- pipeline/transform/transform_gdp.py
+++ pipeline/transform/transform_gdp.py
@@ -1,27 +1,9 @@
 import pandas as pd
 import io
-from utils.common import download_from_azure, upload_to_azure, df_to_bytesio
+from utils.common import df_to_bytesio, download_from_cloud, upload_to_cloud
+from config.config import CLEAN_CONTAINER, FINAL_CONTAINER
 
-def transform_gdp_data():
-    """
-    Loads clean GDP data from Azure Blob Storage
-    Transforms the data to fit facts and dimensions schema:
-    - facts_gdp
-    - dim_geography
-    Uploads the transformed data to Azure Blob Storage
-    """
-    print("Transforming GDP data...\n")
-    cleaned_container = "cleaned-data"
-    clean_gdp_blob_name = "GDP-data/cleaned_gdp_data.csv"
-
-    # Download the cleaned GDP data from Azure Blob Storage
-    gdp_data = download_from_azure(clean_gdp_blob_name, cleaned_container)
-
-    # Read the CSV file into a DataFrame
-    print(f"Reading cleaned GDP data from {clean_gdp_blob_name}...")
-    df = pd.read_csv(gdp_data, encoding="utf-8")
-    print(f"Cleaned GDP data has {len(df)} rows and {len(df.columns)} columns.")
-
+def create_dim_geography(df):
     dim_geography = df[['geofips', 'geo_name', 'region']].drop_duplicates()
     dim_geography = dim_geography.reset_index(drop=True)
 
@@ -63,24 +45,47 @@ def transform_gdp_data():
     dim_geography['project_state'] = dim_geography['project_state'].astype(pd.StringDtype("pyarrow"))
     dim_geography['project_county_name'] = dim_geography['project_county_name'].astype(pd.StringDtype("pyarrow"))
 
+    return dim_geography
+
+def create_facts_gdp(df):
     facts_gdp = df[['facts_gdp_id', 'year_id', 'real_gdp', 'chain_type_index_gdp', 'current_dollar_gdp', 'geofips']]
-    # Reset the index
     facts_gdp.reset_index(drop=True, inplace=True)
-    # Re order the columns
     facts_gdp = facts_gdp[['facts_gdp_id', 'geofips', 'year_id', 'chain_type_index_gdp', 'current_dollar_gdp', 'real_gdp']]
-    
+    return facts_gdp
+
+def transform_gdp_data():
+    """
+    Loads clean GDP data from cloud storage.
+    Transforms the data to fit facts and dimensions schema:
+    - facts_gdp
+    - dim_geography
+    Uploads the transformed data to cloud storage.
+    """
+    print("Transforming GDP data...\n")
+    clean_gdp_blob_name = "GDP-data/cleaned_gdp_data.csv"
+
+    # Download the cleaned GDP data from cloud storage
+    gdp_data = download_from_cloud(blob_name=clean_gdp_blob_name, container_name=CLEAN_CONTAINER)
+
+    # Read the CSV file into a DataFrame
+    print(f"Reading cleaned GDP data from {clean_gdp_blob_name}...")
+    df = pd.read_csv(gdp_data, encoding="utf-8")
+    print(f"Cleaned GDP data has {len(df)} rows and {len(df.columns)} columns.")
+
+    facts_gdp = create_facts_gdp(df)
+    dim_geography = create_dim_geography(df)
+
     print(f"Transformed facts_gdp has {len(facts_gdp)} rows and {len(facts_gdp.columns)} columns.")
     print(f"Transformed dim_geography has {len(dim_geography)} rows and {len(dim_geography.columns)} columns.")
 
-    # Upload dim_geography and facts_gdp to Azure Blob Storage
-    final_container = "final-data"
+    # Upload dim_geography and facts_gdp to cloud storage
     dim_geography_blob_name = "dim_geography.csv"
     facts_gdp_blob_name = "facts_gdp.csv"
     
     output_geography = df_to_bytesio(dim_geography, index=False, encoding='utf-8')
-    upload_to_azure(output_geography, dim_geography_blob_name, final_container)
+    upload_to_cloud(data=output_geography, blob_name=dim_geography_blob_name, container_name=FINAL_CONTAINER)
 
     output_gdp = df_to_bytesio(facts_gdp, index=False, encoding='utf-8')
-    upload_to_azure(output_gdp, facts_gdp_blob_name, final_container)
+    upload_to_cloud(data=output_gdp, blob_name=facts_gdp_blob_name, container_name=FINAL_CONTAINER)
 
     print("GDP data transformation completed.\n")
