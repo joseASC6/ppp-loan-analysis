7c5adea6dd01370ed3e9f584f53ef0ac564428fb
joseASC6 <jose.mauricio0226@gmail.com>
2025-10-09 21:12:14 -0400

    Enhance NAICS transform to merge 2017 fallback codes
    The transformation now builds dim_naics primarily from 2022 data, appending NAICS codes from 2017 only if they are missing in 2022. This ensures the final dimension table includes all relevant codes while prioritizing the latest data.

--- pipeline/transform/transform_naics.py
+++ pipeline/transform/transform_naics.py
@@ -12,22 +12,32 @@ def transform_naics_data():
     print("Transforming NAICS data...\n")
     # Download the cleaned NAICS data from Azure Blob Storage
     # naics_data files: cleaned_naics_data_2022.csv, cleaned_naics_data_2017.csv
-    
-    clean_naics_blob_name = f"{NAICS_FOLDER}/cleaned_naics_data.csv"
-    naics_data = download_from_cloud(blob_name=clean_naics_blob_name, container_name=CLEAN_CONTAINER)
+    # the dim_naics will be based on the 2022 data, 2017 will be added only if a naics_code is missing in the 2022 data
+    # naics_files = ['cleaned_naics_data_2022.csv', 'cleaned_naics_data_2017.csv']
+
+    clean_naics_2022_blob_name = f"{NAICS_FOLDER}/cleaned_naics_data_2022.csv"
+    naics_2022_data = download_from_cloud(blob_name=clean_naics_2022_blob_name, container_name=CLEAN_CONTAINER)
 
     # Read the CSV file into a DataFrame
-    print(f"Reading cleaned NAICS data from {clean_naics_blob_name}...")
-    df = pd.read_csv(naics_data, encoding="utf-8")
-    print(f"Cleaned NAICS data has {len(df)} rows and {len(df.columns)} columns.")
+    print(f"Reading cleaned NAICS data from {clean_naics_2022_blob_name}...")
+    dim_naics = pd.read_csv(naics_2022_data, encoding="utf-8")
+
+    # Get the 2017 data
+    clean_naics_2017_blob_name = f"{NAICS_FOLDER}/cleaned_naics_data_2017.csv"
+    naics_2017_data = download_from_cloud(blob_name=clean_naics_2017_blob_name, container_name=CLEAN_CONTAINER)
+    naics_2017_df = pd.read_csv(naics_2017_data, encoding="utf-8")
+    # Append the rows from 2017 where naics_code is in 2017 and not in 2022
+    # Rows in 2017 but not in 2022
+    naics_fallback_df = naics_2017_df[~naics_2017_df['naics_code'].isin(dim_naics['naics_code'])]
+    dim_naics = pd.concat([dim_naics, naics_fallback_df], ignore_index=True)
 
     # Reset index 
-    df.reset_index(drop=True, inplace=True)
+    dim_naics.reset_index(drop=True, inplace=True)
 
-    print(f"Transformed dim_naics has {len(df)} rows and {len(df.columns)} columns.")
+    print(f"Transformed dim_naics has {len(dim_naics)} rows and {len(dim_naics.columns)} columns.")
 
     # Upload the DataFrame to Azure Blob Storage
     final_blob_name = "dim_naics.csv"
-    output = df_to_bytesio(df, index=False, encoding='utf-8')
+    output = df_to_bytesio(dim_naics, index=False, encoding='utf-8')
     upload_to_cloud(data=output, blob_name=final_blob_name, container_name=FINAL_CONTAINER)
     print("NAICS data transformation completed.\n")
